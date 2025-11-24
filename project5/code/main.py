a1aa42b20233e83a0fad1fefa168089b2d47195a
hamedheli <hamedhelisaz@gmail.com>
2025-11-23 23:15:29 -0800

    completing project 5

--- project5/code/main.py
+++ project5/code/main.py
@@ -110,13 +110,54 @@ def q1_2():
 
     sigmas = 10.0 ** np.array([-2, -1, 0, 1, 2])
     lammys = 10.0 ** np.array([-4, -3, -2, -1, 0, 1, 2])
-
-    # train_errs[i, j] should be the train error for sigmas[i], lammys[j]
+    
     train_errs = np.full((len(sigmas), len(lammys)), 100.0)
     val_errs = np.full((len(sigmas), len(lammys)), 100.0)  # same for val
+    optimizer = GradientDescentLineSearch()
+    for i in range(len(sigmas)):
+        for j in range(len(lammys)):
+            loss_fn = KernelLogisticRegressionLossL2(lammys[j])
+            kernel_rbf = GaussianRBFKernel(sigmas[i])
+            klr_model_rbf = KernelClassifier(loss_fn, optimizer, kernel_rbf)
+            klr_model_rbf.fit(X_train, y_train)
+            train_errs[i, j] = np.mean(klr_model_rbf.predict(X_train) != y_train)
+            val_errs[i, j] = np.mean(klr_model_rbf.predict(X_val) != y_val)
 
-    """YOUR CODE HERE FOR Q1.2"""
-    pass
+
+    # Hyperparameters for minimum lambda and sigma
+    ## Training Error
+    min_train_err_inx = np.unravel_index(np.argmin(train_errs, axis=None), train_errs.shape)
+    min_train_error_sigma = sigmas[min_train_err_inx[0]]
+    min_train_error_lammy = lammys[min_train_err_inx[1]]
+    min_train_error = train_errs[min_train_err_inx]
+
+    print(f"Sigma for minimum training error is {min_train_error_sigma:.4f}")
+    print(f"Lambda for minimum training error is {min_train_error_lammy:.4f}")
+    print(f"Minimum training error is {min_train_error:.1%}")
+
+    loss_fn = KernelLogisticRegressionLossL2(min_train_error_lammy)
+    kernel_rbf = GaussianRBFKernel(min_train_error_sigma)
+    klr_model_rbf_min_training_error = KernelClassifier(loss_fn, optimizer, kernel_rbf)
+    klr_model_rbf_min_training_error.fit(X_train, y_train)
+    fig = plot_classifier(klr_model_rbf_min_training_error, X_train, y_train)
+    savefig("logRegRBF_min_train_error.png", fig)
+
+    ## Validation Error
+    min_val_err_inx = np.unravel_index(np.argmin(val_errs, axis=None), val_errs.shape)
+    min_val_error_sigma = sigmas[min_val_err_inx[0]]
+    min_val_error_lammy = lammys[min_val_err_inx[1]]
+    min_valid_error = val_errs[min_val_err_inx]
+
+    print(f"Sigma for minimum validation error is {min_val_error_sigma:.4f}")
+    print(f"Lambda for minimum validation error is {min_val_error_lammy:.4f}")
+    print(f"Minimum validation error is {min_valid_error:.1%}")
+
+    loss_fn = KernelLogisticRegressionLossL2(min_val_error_lammy)
+    kernel_rbf = GaussianRBFKernel(min_val_error_sigma)
+    klr_model_rbf_min_valid_error = KernelClassifier(loss_fn, optimizer, kernel_rbf)
+    klr_model_rbf_min_valid_error.fit(X_train, y_train)
+    fig = plot_classifier(klr_model_rbf_min_valid_error, X_train, y_train)
+    savefig("logRegRBF_min_valid_error.png", fig)
 
     # Make a picture with the two error arrays. No need to worry about details here.
     fig, axes = plt.subplots(1, 2, figsize=(12, 5), constrained_layout=True)
@@ -134,7 +175,6 @@ def q1_2():
         ax.xaxis.set_ticks_position("bottom")
     fig.colorbar(cax)
     savefig("logRegRBF_grids.png", fig)
-
 
 @handle("3.2")
 def q3_2():
@@ -166,8 +206,35 @@ def q3_2():
     savefig("animals_random.png", fig)
     plt.close(fig)
 
-    """YOUR CODE HERE FOR Q3"""
-    pass
+    PCA_encoder = PCAEncoder(2)
+    PCA_encoder.fit(X_train_standardized)
+    W = PCA_encoder.W
+    Z = X_train_standardized @ W.T
+    fig, ax = plt.subplots()
+    ax.scatter(Z[:, 0], Z[:, 1])
+    for i in random_is:
+        xy = Z[i, [0, 1]]
+        ax.annotate(animal_names[i], xy=xy)
+    savefig("animals_PCA.png", fig)
+    plt.close(fig)
+
+    print(f"the trait with largest absolute value of Z1 is {trait_names[np.argmax(np.abs(W), axis = 1)[0]]}")
+    print(f"the trait with largest absolute value of Z2 is {trait_names[np.argmax(np.abs(W), axis = 1)[1]]}")
+
+    reconstruction_loss = np.linalg.norm(Z@W-X_train_standardized)**2
+    X_train_standardized_norm = np.linalg.norm(X_train_standardized)**2
+    print(f"variance explained is {1-reconstruction_loss/X_train_standardized_norm:.2%}")
+
+    PCA_encoder = PCAEncoder(5)
+    PCA_encoder.fit(X_train_standardized)
+    W = PCA_encoder.W
+    Z = X_train_standardized @ W.T
+    reconstruction_loss = np.linalg.norm(Z@W-X_train_standardized)**2
+    X_train_standardized_norm = np.linalg.norm(X_train_standardized)**2
+    print(f"variance explained is {1-reconstruction_loss/X_train_standardized_norm:.2%}")
+
+    
+
 
 
 @handle("4")
@@ -200,8 +267,19 @@ def q4_1():
     X_train, mu, sigma = standardize_cols(X_train_orig)
     X_val, _, _ = standardize_cols(X_val_orig, mu, sigma)
 
-    """YOUR CODE HERE FOR Q4.1"""
-    pass
+    loss = LeastSquaresLoss()
+    learning_rate = ConstantLR(0.0003)
+    for i in [1,10,100]:
+        optimizer = StochasticGradient (learning_rate_getter=learning_rate, 
+                                    batch_size=i, 
+                                    base_optimizer=GradientDescent(),
+                                    max_evals= 10)
+        model  = LinearModel(loss, optimizer)
+        model.fit(X_train, y_train)
+        train_error = np.mean((model.predict(X_train) - y_train)**2)
+        valid_error = np.mean((model.predict(X_val) - y_val)**2)
+        print(f"Training error (MSE) for batch size of {i} is {train_error:.3f}")
+        print(f"Validation error (MSE) for batch size of {i} is {valid_error:.3f}")
 
 
 @handle("4.3")
@@ -210,8 +288,32 @@ def q4_3():
     X_train, mu, sigma = standardize_cols(X_train_orig)
     X_val, _, _ = standardize_cols(X_val_orig, mu, sigma)
 
-    """YOUR CODE HERE FOR Q4.3"""
-    pass
+    loss = LeastSquaresLoss()
+    c= 0.1
+    learning_rate_CLR = ConstantLR(c)
+    learning_rate_ILR = InverseLR(c)
+    learning_rate_ISLR = InverseSquaredLR(c)
+    learning_rate_ISqrtLR = InverseSqrtLR(c)
+
+    for (learning_rate, names) in [(learning_rate_CLR,"CLR"), 
+                                   (learning_rate_ILR, "ILR"), 
+                                   (learning_rate_ISLR, "ISLR"), 
+                                   (learning_rate_ISqrtLR, "ISqrtLR")]:
+        optimizer = StochasticGradient (learning_rate_getter=learning_rate, 
+                                    batch_size=10, 
+                                    base_optimizer=GradientDescent(),
+                                    max_evals= 50)
+        model  = LinearModel(loss, optimizer)
+        model.fit(X_train, y_train)
+        fig, ax = plt.subplots()
+        ax.plot(model.fs, marker="o")
+        ax.set_xlabel("Gradient descent iterations")
+        ax.set_ylabel("Objective function f value")
+        ax.set_title(f"learning rate is {names}")
+        dynamic_filename = f'gd_curve_iteration_{names}.png'
+        savefig(dynamic_filename, fig)
+
+
 
 
 if __name__ == "__main__":
